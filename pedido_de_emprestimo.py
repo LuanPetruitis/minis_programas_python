71156345d50acd4d0b6a069f7d68dd97b046e4df
Luan Petruitis <luanpetruitis@hotmail.com>
2020-04-06 16:43:18 -0300

    emprestimo
    melhorar o calculo

--- pedido_de_emprestimo.py
+++ pedido_de_emprestimo.py
@@ -3,7 +3,7 @@ salario = float(input('qual é o seu salário:  R$'))
 tempo = float(input('quantos anos você vai pagar:  '))
 entrada = float(input('qual é o valor que você pode dar de entrada'))
 p = preco / (tempo*12)
-if p <= salario * (30/100):
+if p <= (salario - entrada) * (30/100):
     print('O empréstimo será consedido, você deve pagar R${}'. format(p))
 else:
     print('O emprestimo não será concedido')

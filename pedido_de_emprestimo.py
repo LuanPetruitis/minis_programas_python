6257e249874e85f954f01d13ac0b3cf6f9ffaad7
Luan Petruitis <luanpetruitis@hotmail.com>
2020-04-06 16:39:40 -0300

    emprestimo
    Conceder ou não um emprestimo para comprar uma casa.  Será concedido se o salário for maior ou igual a 30 porcento do valor restante

---
+++ pedido_de_emprestimo.py
@@ -0,0 +1,9 @@
+preco = float(input('qual é o valor da casa:  R$ '))
+salario = float(input('qual é o seu salário:  R$'))
+tempo = float(input('quantos anos você vai pagar:  '))
+entrada = float(input('qual é o valor que você pode dar de entrada'))
+p = preco / (tempo*12)
+if p <= salario * (30/100):
+    print('O empréstimo será consedido, você deve pagar R${}'. format(p))
+else:
+    print('O emprestimo não será concedido')

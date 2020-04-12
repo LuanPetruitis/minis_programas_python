11f60af8d1e65bfe2d58ed423bc578da56902f90
Luan Petruitis <luanpetruitis@hotmail.com>
2020-04-12 11:43:44 -0300

    mudança
    otmização do código

--- tabuada.py
+++ tabuada.py
@@ -1,6 +1,4 @@
 e = int(input('escolha um numero inteiro para fazer a tabuada:  '))
-n = 0
-for c in range(n, 11):
-    t = e * n
-    print('{} * {} = {}'.format(e, n, t))
-    n += 1
+for c in range(11):
+   t = e * c
+   print(f'{e} * {c} = {t}')

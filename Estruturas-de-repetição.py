adbd5dbdf2423191e682619e6513ebbce665761c
LuanPetruitis <luanpetruitis@hotmail.com>
2021-01-27 15:54:00 -0300

    Pronto

--- Estruturas-de-repetição.py
+++ Estruturas-de-repetição.py
@@ -2,6 +2,6 @@ for c in range(0, 10):
     print(f'{c}')
 
 
-# Forma para deixar um decimal na quantidade de casas decimais que você quer
+# Forma para deixar um decimal com quantidade de casas decimais que você quer
 n = 2.34556
 print(f'{n:.2f}')

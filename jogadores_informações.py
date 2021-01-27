b085099761fb16f7dbb928b5bd80639abec338d2
LuanPetruitis <luanpetruitis@hotmail.com>
2021-01-27 15:35:12 -0300

    Mudanças no jogadores_informações

--- jogadores_informações.py
+++ jogadores_informações.py
@@ -2,13 +2,14 @@ time = list()
 jogador = dict()
 partidas = list()
 
+# Adiciona os jogadores
 while True:
     jogador.clear()
     jogador['nome'] = str(input('Nome do jogador:  '))
     tot = int(input(f'Quantas partidas o {jogador["nome"]} jogou?  '))
     partidas.clear()
     for c in range(0, tot):
-        partidas.append(int(input(f'     Quantos gols na partida {c+1}? ')))
+        partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
     time.append(jogador.copy())
@@ -19,20 +20,28 @@ while True:
         print('ERRO! Responda apenas S ou N.')
     if resp == 'N':
         break
+
+# Digita em tela
 print('-='*30)
+
+# Digita linha do nome dos dados
 print('cod', end='')
 for i in jogador.keys():
-    print(f'{i:<15}', end='')
+    print(f'   {i:<15}', end='')
 print()
-print('-'*40)
+
+# Digita linhas dos jogadores
+print('-'*50)
 for k, v in enumerate(time):
     print(f'{k:>3} ', end='')
     for d in v.values():
-        print(f'{str(d):<15}', end='')
+        print(f'   {str(d):<15}', end='')
     print()
-print('-'*40)
+print('-'*50)
+
+# Procura ou finaliza
 while True:
-    busca = int(input('Mostrar dados de qual jogador? [999 parar]   '))
+    busca = int(input('Mostrar dados de qual jogador digite o número do índice? [999 parar]   '))
     if busca == 999:
         break
     if busca >= len(time):
@@ -40,6 +49,6 @@ while True:
     else:
         print(f'-- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}: ')
         for i, g in enumerate(time[busca]['gols']):
-            print(f'    No jogo {i} fez {g} gols.')
+            print(f'    No jogo {i+1} fez {g} gols.')
     print('-'*40)
 print('<<< VOLTE SEMPRE >>>')
